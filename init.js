052deb39ef9a70d052e313fb3bf051e88d6ed1fa
Abdoul Aziz BA <aaziz.ba2@univ-thies.sn>
2022-02-18 17:00:16 +0100

    first-commit

---
+++ init.js
@@ -0,0 +1,14 @@
+(function ($) {
+  $(function () {
+
+    $('.sidenav').sidenav();
+    $('.parallax').parallax();
+
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
+
+
+$('.carousel.carousel-slider').carousel({
+  fullWidth: true,
+  indicators: true
+});
